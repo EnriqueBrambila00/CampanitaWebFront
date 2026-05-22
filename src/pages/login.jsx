54f0497f7d9991b68213165a8f2ebeef8ad2d784
Enrique Gómez Brambila <lg330000@gmail.com>
2026-05-22 15:42:47 -0600

    remove axios, error de importacion

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import axios from 'axios';
 
 export function Login() {
   const [correo, setCorreo] = useState('');
